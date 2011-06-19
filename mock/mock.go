d6bf0d46c7ccced6b6fd445dda060c0b3acfefe7
Kyle Lemons <kyle@kylelemons.net>
2011-06-19 09:31:55 -0700

    Moved the package writing into gimmock

--- mock/mock.go
+++ mock/mock.go
@@ -2,30 +2,46 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
+	"os"
 
 	"github.com/kylelemons/gimmock"
+)
+
+var (
+	pkg = flag.String("pkg", "", "Package for output file")
 )
 
 func main() {
 	flag.Parse()
 
+	var mocks []*gimmock.Mock
+	var outpkg string
+
 	for _, file := range flag.Args() {
-		pkg, mocks, err := gimmock.Process(file)
+		filepkg, filemocks, err := gimmock.Process(file)
 		if err != nil {
 			log.Fatalf("mock: %s", err)
 		}
-		fmt.Printf("package %s\n", pkg)
 
-		fmt.Printf("import (\n")
-		for _, imp := range gimmock.MockDependencies {
-			fmt.Printf("\t%q\n", imp)
+		if *pkg == "" {
+			if outpkg == "" {
+				outpkg = filepkg
+			} else {
+				if filepkg != outpkg {
+					log.Fatalf("mock: file %s does not match package %s", file, filepkg)
+				}
+			}
+		} else {
+			outpkg = *pkg
 		}
-		fmt.Printf(")\n")
 
-		for _, mock := range mocks {
-			fmt.Printf("%s\n", mock.MockClass())
-		}
+		mocks = append(mocks, filemocks...)
 	}
+
+	if len(mocks) == 0 {
+		log.Fatalf("mock: no interfaces found")
+	}
+
+	gimmock.WriteMockFile(os.Stdout, outpkg, mocks)
 }
